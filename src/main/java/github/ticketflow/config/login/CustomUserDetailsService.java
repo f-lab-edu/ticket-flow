47112afb394fc8bbb177bf9fa842f20a814921ce
hyeok96 <saing0813@naver.com>
2024-08-31 21:54:07 +0900

    Feat: [#4] spring security userDetails, userDetailsService 구현

---
+++ src/main/java/github/ticketflow/config/login/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package github.ticketflow.config.login;
+
+import github.ticketflow.config.exception.ErrorCode;
+import github.ticketflow.config.exception.GlobalCommonException;
+import github.ticketflow.domian.user.UserEntity;
+import github.ticketflow.domian.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByEmail(email).orElseThrow(() -> new GlobalCommonException(ErrorCode.NOT_FOUND_EMAIL));
+        return new CustomUserDetails(userEntity);
+    }
+}
