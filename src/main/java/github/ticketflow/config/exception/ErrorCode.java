7561b7610e1defa718ba48131d0180c760f50fee
hyeok96 <saing0813@naver.com>
2024-08-31 20:37:49 +0900

    Feat: ErrorCode에 token, user 관련 ErrorCode 추가

--- src/main/java/github/ticketflow/config/exception/ErrorCode.java
+++ src/main/java/github/ticketflow/config/exception/ErrorCode.java
@@ -8,7 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다.");
+    // user error
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
+    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, "이메일을 찾을 수 없습니다."),
+
+    // token error
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    ABNORMAL_TOKEN(HttpStatus.BAD_REQUEST, "토큰이 없거나 정상적이지 않은 토큰입니다.");
+
+
 
     private final HttpStatus status;
     private final String message;
