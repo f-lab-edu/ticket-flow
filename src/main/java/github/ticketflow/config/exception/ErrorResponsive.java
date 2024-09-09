54d718300dc23674da79b2d653bc65c2d16fa616
hyeok96 <saing0813@naver.com>
2024-09-09 20:19:08 +0900

    Feat: [#9] ErrorCode를 ErrorResponsive 변경

--- src/main/java/github/ticketflow/config/exception/ErrorResponsive.java
+++ src/main/java/github/ticketflow/config/exception/ErrorResponsive.java
@@ -2,7 +2,7 @@ package github.ticketflow.config.exception;
 
 import org.springframework.http.HttpStatus;
 
-public interface ErrorCode {
+public interface ErrorResponsive {
 
     HttpStatus getStatus();
     String getMessage();
--- src/main/java/github/ticketflow/config/exception/category/CategoryErrorResponsive.java
+++ src/main/java/github/ticketflow/config/exception/category/CategoryErrorResponsive.java
@@ -1,13 +1,13 @@
 package github.ticketflow.config.exception.category;
 
-import github.ticketflow.config.exception.ErrorCode;
+import github.ticketflow.config.exception.ErrorResponsive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum CategoryErrorCode implements ErrorCode {
+public enum CategoryErrorResponsive implements ErrorResponsive {
 
     NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다.");
 
