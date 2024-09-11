554f5ffeb01b3097e754096834d067cc157dbf87
hyeok96 <saing0813@naver.com>
2024-09-12 00:43:50 +0900

    Feat: [#14] EventLocation Request, Response, Update DTO 생성

---
+++ src/main/java/github/ticketflow/domian/eventLocation/dto/EventLocationRequestDTO.java
@@ -0,0 +1,18 @@
+package github.ticketflow.domian.eventLocation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventLocationRequestDTO {
+
+    @NotBlank
+    private String eventLocationName;
+    @NotBlank
+    private int totalSeats;
+
+}
