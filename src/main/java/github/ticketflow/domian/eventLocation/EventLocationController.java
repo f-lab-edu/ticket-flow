0d274fd2d00b4a2de89cfc333af4e8d5fb722acb
hyeok96 <saing0813@naver.com>
2024-09-13 22:44:37 +0900

    Modify: [#17] controller에 RequestMapping 추가

--- src/main/java/github/ticketflow/domian/eventLocation/EventLocationController.java
+++ src/main/java/github/ticketflow/domian/eventLocation/EventLocationController.java
@@ -12,27 +12,28 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/event-location")
 public class EventLocationController {
 
     private final EventLocationService eventLocationService;
 
-    @GetMapping("/event-location/{eventLocationId}")
+    @GetMapping("/{eventLocationId}")
     public ResponseEntity<EventLocationResponseDTO> getEventLocationById(@PathVariable Long eventLocationId) {
         return ResponseEntity.ok(eventLocationService.getEventLocation(eventLocationId));
     }
 
-    @PostMapping("/event-location")
+    @PostMapping()
     public ResponseEntity<EventLocationResponseDTO> createEventLocation(@RequestBody EventLocationRequestDTO dto) {
         return ResponseEntity.ok(eventLocationService.createEventLocation(dto));
     }
 
-    @PatchMapping("/event-location/{eventLocationId}")
+    @PatchMapping("/{eventLocationId}")
     public ResponseEntity<EventLocationResponseDTO> updateEventLocation(@PathVariable Long eventLocationId,
                                                                         @RequestBody EventLocationUpdateRequestDTO dto) {
         return ResponseEntity.ok(eventLocationService.updateEventLocation(eventLocationId, dto));
     }
 
-    @DeleteMapping("/event-location/{eventLocationId}")
+    @DeleteMapping("/{eventLocationId}")
     public ResponseEntity<EventLocationResponseDTO> deleteEventLocation(@PathVariable Long eventLocationId) {
         return ResponseEntity.ok(eventLocationService.deletedEventLocation(eventLocationId));
     }
