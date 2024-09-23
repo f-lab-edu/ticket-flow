a871e1e3f0b44353ae6e06c96133601028ea6daf
hyeok96 <saing0813@naver.com>
2024-09-23 14:05:39 +0900

    Feat: [#23] EventController EventFacade 를 위한 리팩토링

--- src/main/java/github/ticketflow/domian/event/EventController.java
+++ src/main/java/github/ticketflow/domian/event/EventController.java
@@ -1,8 +1,8 @@
 package github.ticketflow.domian.event;
 
 import github.ticketflow.domian.event.dto.EventRequestDTO;
+import github.ticketflow.domian.event.dto.EventResponseDTO;
 import github.ticketflow.domian.event.dto.EventUpdateRequestDTO;
-import github.ticketflow.domian.event.entity.EventEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,32 +15,32 @@ import java.util.List;
 @RequestMapping("/event")
 public class EventController {
 
-    private final EventService eventService;
+    private final EventFacade eventFacade;
 
     @GetMapping("/{eventId}")
-    public ResponseEntity<EventEntity> getEventById(@PathVariable Long eventId) {
-        return ResponseEntity.ok(eventService.getEventById(eventId));
+    public ResponseEntity<EventResponseDTO> getEventById(@PathVariable Long eventId) {
+        return ResponseEntity.ok(eventFacade.getEventById(eventId));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<EventEntity>> getEventByCategoryId(@PathVariable Long categoryId,
+    public ResponseEntity<List<EventResponseDTO>> getEventByCategoryId(@PathVariable Long categoryId,
                                                                   @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo) {
-        return ResponseEntity.ok(eventService.getEventByCategoryId(categoryId, pageNo));
+        return ResponseEntity.ok(eventFacade.getEventByCategoryId(categoryId, pageNo));
     }
 
     @PostMapping
-    public ResponseEntity<EventEntity> createEvent(@Valid @RequestBody EventRequestDTO dto) {
-        return ResponseEntity.ok(eventService.createEvent(dto));
+    public ResponseEntity<EventResponseDTO> createEvent(@Valid @RequestBody EventRequestDTO dto) {
+        return ResponseEntity.ok(eventFacade.createEvent(dto));
     }
 
     @PatchMapping("/{eventId}")
-    public ResponseEntity<EventEntity> updateEvent(@PathVariable Long eventId,
+    public ResponseEntity<EventResponseDTO> updateEvent(@PathVariable Long eventId,
                                                     @Valid @RequestBody EventUpdateRequestDTO dto) {
-        return ResponseEntity.ok(eventService.updateEvent(eventId, dto));
+        return ResponseEntity.ok(eventFacade.updateEvent(eventId, dto));
     }
 
     @DeleteMapping("/{eventId}")
-    public ResponseEntity<EventEntity> deleteEvent(@PathVariable Long eventId) {
-        return ResponseEntity.ok(eventService.deletedEvent(eventId));
+    public ResponseEntity<EventResponseDTO> deleteEvent(@PathVariable Long eventId) {
+        return ResponseEntity.ok(eventFacade.deletedEvent(eventId));
     }
 }
