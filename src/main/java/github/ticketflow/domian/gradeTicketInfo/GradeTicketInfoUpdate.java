865fe0827835dd275cedea206171558f613824d0
hyeok96 <saing0813@naver.com>
2024-09-29 23:20:48 +0900

    Modify: [#26] GradeTicketInfoUpdate 필드 @Nullable 추가

--- src/main/java/github/ticketflow/domian/gradeTicketInfo/GradeTicketInfoUpdate.java
+++ src/main/java/github/ticketflow/domian/gradeTicketInfo/GradeTicketInfoUpdate.java
@@ -4,6 +4,7 @@ import github.ticketflow.domian.event.EventEntity;
 import github.ticketflow.domian.eventLocation.EventLocationEntity;
 import github.ticketflow.domian.seatGrade.SeatGradeEntity;
 
+import jakarta.annotation.Nullable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -15,8 +16,11 @@ import lombok.Builder;
 @Builder
 public class GradeTicketInfoUpdate {
 
+    @Nullable
     private EventEntity eventEntity;
+    @Nullable
     private EventLocationEntity eventLocationEntity;
+    @Nullable
     private SeatGradeEntity seatGradeEntity;
 
 }
