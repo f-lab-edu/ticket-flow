0209a43be162a44742c1afccd63a5fbd63037ba9
hyeok96 <saing0813@naver.com>
2024-10-07 16:31:06 +0900

    Feat: [#38] EventRepository 에 findAllByEventNameContainingIgnoreCase 메소드 생성

--- src/main/java/github/ticketflow/domian/event/EventRepository.java
+++ src/main/java/github/ticketflow/domian/event/EventRepository.java
@@ -2,5 +2,10 @@ package github.ticketflow.domian.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    List<EventEntity> findAllByEventNameContainingIgnoreCase(String eventName);
+
 }
