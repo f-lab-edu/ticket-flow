e8a9334275ec442bb31ed26fa6c0979363b4f03f
hyeok96 <saing0813@naver.com>
2024-10-07 16:47:22 +0900

    Feat: [#38] PopularSearchServiceTest 테스트 코드 작성

---
+++ src/test/java/github/ticketflow/domian/popularSearch/PopularSearchServiceTest.java
@@ -0,0 +1,44 @@
+package github.ticketflow.domian.popularSearch;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PopularSearchServiceTest {
+
+    @Autowired
+    private PopularSearchService popularSearchService;
+
+    @DisplayName("검색어가 저장이 되고 인기 검색어를 불러오면 리스트로 나온다.")
+    @Test
+    void getPopularKeywordsTest() {
+        // give
+        popularSearchService.incrementKeywordCount("서울");
+        popularSearchService.incrementKeywordCount("수원");
+        popularSearchService.incrementKeywordCount("수원");
+        popularSearchService.incrementKeywordCount("서울");
+        popularSearchService.incrementKeywordCount("수원");
+
+        // when
+        List<String> result = popularSearchService.getPopularKeywords(10);
+
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0)).isEqualTo("수원");
+        assertThat(result.get(1)).isEqualTo("서울");
+    }
+
+
+}
