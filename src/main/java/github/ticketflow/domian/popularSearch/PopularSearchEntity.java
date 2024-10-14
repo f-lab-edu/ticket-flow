28ec80b61c7a54ece796b3034ef1cd4b5b326826
hyeok96 <saing0813@naver.com>
2024-10-14 20:41:35 +0900

    Feat : [#41] PopularSearchEntity 생성

---
+++ src/main/java/github/ticketflow/domian/popularSearch/PopularSearchEntity.java
@@ -0,0 +1,38 @@
+package github.ticketflow.domian.popularSearch;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PopularSearch")
+public class PopularSearchEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "popular_search_id")
+    private Long PopularSearchId;
+
+    @Column(name = "keyword", nullable = false, unique = true)
+    private String keyword;
+
+    @Column(name = "search_count")
+    private int searchCount;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDate createdAt;
+
+}
