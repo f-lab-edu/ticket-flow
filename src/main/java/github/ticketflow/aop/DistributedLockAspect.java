f4abc4a20c5ae179c247248b2268d64b0f9ca79f
hyeok96 <saing0813@naver.com>
2024-10-30 03:42:01 +0900

    Feat: [#44] Redisson AOP Config 셋팅

---
+++ src/main/java/github/ticketflow/aop/DistributedLockAspect.java
@@ -0,0 +1,41 @@
+package github.ticketflow.aop;
+
+import github.ticketflow.config.RedissonConfig;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DistributedLockAspect {
+
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(distributedLock)")
+    public Object lock(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
+        String lockKey = distributedLock.key();
+        RLock lock = redissonClient.getLock(lockKey);
+
+        boolean isLocked = false;
+        try {
+            isLocked = lock.tryLock(5, 10, TimeUnit.SECONDS);
+            if (isLocked) {
+                return joinPoint.proceed();
+            } else {
+                throw new IllegalStateException("Could not acquire lock for key: " + lockKey);
+            }
+        } finally {
+            if (isLocked) {
+                lock.unlock();  // 메서드 실행 후 락 해제
+            }
+        }
+    }
+
+}
