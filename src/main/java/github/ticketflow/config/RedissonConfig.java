0d31c4b1bda08dfb93f16eeef3c93f38062a7b93
hyeok96 <saing0813@naver.com>
2024-11-12 00:25:43 +0900

    Feat: [#53] RedissonConfig redis cache 설정

--- src/main/java/github/ticketflow/config/RedissonConfig.java
+++ src/main/java/github/ticketflow/config/RedissonConfig.java
@@ -3,15 +3,25 @@ package github.ticketflow.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedissonConfig {
 
     @Bean
-    @Profile("!test")
+    @Profile("test")
     public RedissonClient redisson() {
         Config config = new Config();
         config.useSingleServer()
@@ -22,4 +32,20 @@ public class RedissonConfig {
         return Redisson.create(config);
     }
 
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("my-cache-server", 6379);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(7))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
 }
